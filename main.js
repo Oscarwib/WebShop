3c4993c96af2e0a80d06d655af0fd1387bb9759c
Adam Jenvald <adam.jenvald@gmail.com>
2025-01-09 10:30:20 +0100

    mergetime2

--- main.js
+++ main.js
@@ -4,13 +4,21 @@ document.addEventListener("DOMContentLoaded", (event) => {
     const myComponent = document.querySelector('cart-page');
     const shadowRoot = myComponent.shadowRoot;
     const internalElement = shadowRoot.querySelector('.checkout');
-
+    const homepage = document.querySelector('.home-page');
     const pages = document.querySelectorAll('item-page');
-
     const logo = document.querySelector('.logo');
+
     logo.addEventListener('click', () => {
-        window.location.href = '/';
-    });
+        
+        pages.forEach(pg => {
+            pg.hide();
+        })
+        homepage.setAttribute("aria-hidden", "false");
+    })
+        
+    // intr.setAttribute("aria-hidden", "true");
+    // console.log(shadowRoot)
+    
 
 
     item.addEventListener('click', () => {
